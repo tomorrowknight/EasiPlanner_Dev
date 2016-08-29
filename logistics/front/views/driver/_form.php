ebade0546a3ba4eee756bc4466c33ba99ac8d313
Alvin Arulselvan <alvinthedeveloper@gmail.com>
2016-08-29 16:42:05 +0800

    Edited DriverController and _form to remove the need for email input for drivers

--- logistics/front/views/driver/_form.php
+++ logistics/front/views/driver/_form.php
@@ -12,7 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
     <?if ($model->isNewRecord) echo $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>

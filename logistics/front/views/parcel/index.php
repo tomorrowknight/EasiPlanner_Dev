f2e28fa7009ccff8b52eb4fdcf6a8a0cd9662a89
Alvin Arulselvan <alvinthedeveloper@gmail.com>
2016-10-13 14:27:42 +0800

    adding null checks for image and signature

--- logistics/front/views/parcel/index.php
+++ logistics/front/views/parcel/index.php
@@ -68,7 +68,7 @@ $this->params ['breadcrumbs'] [] = $this->title;
 							'format' => 'raw',
 							'attribute' => 'signature',
 							'value' => function ($model) {
-								if ($model->signature == NULL)
+								if ($model->signature == NULL || $model->image == null)
 									return "";
 								return Html::a ( "view", "data:image/jpeg;base64," . $model->signature , ['target'=>'_blank'] );
 							} 
@@ -80,7 +80,7 @@ $this->params ['breadcrumbs'] [] = $this->title;
 							'value' => function ($model) {
 								if (empty ( $model->image )) {
 									return "";
-								} else if ($model->image == NULL) {
+								} else if ($model->image == NULL || $model->image == null) {
 									return "";
 								} else {
 									return Html::a ( "view", "data:image/jpeg;base64," . $model->image , ['target'=>'_blank']);
